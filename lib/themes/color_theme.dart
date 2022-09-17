1103c8146a2e53e89e5c4881859203c1492a9ffe
doch13_ <dohui0922@dimigo.hs.kr>
2022-09-18 00:29:06 +0900

    feat: Color Theme

--- lib/themes/color_theme.dart
+++ lib/themes/color_theme.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/painting.dart';
 
-final redOne = Color(0xFFFF5555);
+final blueOne = Color(0xFFF4F7FC);
+final blueTwo = Color(0xFF4169E1);
+
+final grayOne = Color(0xFFA3A3A3);
+final grayTwo = Color(0xFF555555);
+final grayThree = Color(0xFFC3C3C3);
+
+final blackOne = Color(0xFF333333);
 
 final emptyColor = Color(0xFFFFFF);
