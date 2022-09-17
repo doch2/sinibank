4e5b37c3991c68fd3f71d017fc2489b11aa8c60d
doch13_ <dohui0922@dimigo.hs.kr>
2022-09-18 00:28:26 +0900

    Create text_theme.dart

---
+++ lib/themes/text_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'color_theme.dart';
+
+const loginPage_title = TextStyle(
+    fontSize: 36, color: Colors.black, fontWeight: FontWeight.w700, letterSpacing: 0);
+final loginPage_subTitle = TextStyle(
+    fontSize: 20, color: grayOne, fontWeight: FontWeight.w600, letterSpacing: -0.8);
+final loginPage_loginBtn = TextStyle(
+    fontSize: 16, color: Colors.white, fontWeight: FontWeight.w700, letterSpacing: -0.8);
